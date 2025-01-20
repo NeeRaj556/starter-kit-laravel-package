f6b27656011fe1678dc512d835eb5010a5e7759a
NeeRaj556 <niraj.bj85@gmail.com>
2025-01-20 16:29:11 +0545

    route file missing fixed

---
+++ src/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Middleware\JwtMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/up', function () {
+    return response()->json(['message' => 'Server is up and running!'], 200);
+});
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('user', [AuthController::class, 'getUser']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+Route::apiResource('/products', ProductController::class);
